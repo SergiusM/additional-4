ca57da9409143d31cd58f5c74cc1710a8202c3e2
SergiusM <90497067+SergiusM@users.noreply.github.com>
2022-06-27 14:43:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "lib/bmp.hpp"
+
+int main() {
+    BMP image;
+    image.readBMP("karin.bmp");
+
+    std::string word;
+    std::cin >> word;
+    image.shifr(word);
+    //image.ashifr();
+
+    image.writeBMP("karout.bmp");
+
+    return 0;
+}
